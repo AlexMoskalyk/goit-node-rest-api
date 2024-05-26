6eeffd5b894f6f865ec56fcdaa42595a77433657
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-05-26 15:51:03 +0200

    finished task

--- middlewars/isEmpryBody.js
+++ middlewars/isEmpryBody.js
@@ -3,7 +3,7 @@ import HttpError from "../helpers/HttpError.js";
 const isEmptyBody = (req, res, next) => {
   const { length } = Object.keys(req.body);
   if (!length) {
-    return next(HttpError(400, "Body must have at least one key"));
+    return next(HttpError(400, "Body must have at least one KEY"));
   }
   next();
 };
